1c1c00ef3591b32714067ae768d322c1556edec2
OrangeTree33 <krakaduzaz2@gmail.com>
2022-04-28 18:57:23 +0300

    big

---
+++ main.js
@@ -0,0 +1,39 @@
+const button = document.querySelector('button');
+const paragraph = document.querySelector('h1');
+const calcscreen = document.querySelector('#foor')
+
+// button.addEventListener('click', teste);
+
+function num1(){
+    calcscreen.textContent = "1";
+}
+function num2(){
+    calcscreen.textContent = "2";
+}
+function num3(){
+    calcscreen.textContent = "3";
+}
+function num4(){
+    calcscreen.textContent = "4";
+}
+function num5(){
+    calcscreen.textContent = "5";
+}
+function num6(){
+    calcscreen.textContent = "6";
+}
+function num7(){
+    calcscreen.textContent = "7";
+}
+function num8(){
+    calcscreen.textContent = "8";
+}
+function num9(){
+    calcscreen.textContent = "9";
+    if(calcscreen.textContent= "6"){
+        window.location.replace("mistake.html"); //PLEASE DON'T FORGET TO REMOVE WHEN FINISHEDF PROFULDCT PLEASEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+    }
+}
+function num0(){
+    calcscreen.textContent = "0";
+}
