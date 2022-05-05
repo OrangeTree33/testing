48bcd29323a065d58c75a0704d81a0e807126400
OrangeTree33 <krakaduzaz2@gmail.com>
2022-05-05 21:48:42 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const paragraph = document.querySelector('h1');
 const calcscreen = document.querySelector('#foor')
 var number1= "";
 let numberanswerplus = [];
-let numberanswer = [];
-let lastsymbol = "";
+let numberanswer = 0;
+let tempsymbol = "";
 var intruder = new Boolean(false);
 var operations = 0;
 let testing = [];
@@ -71,74 +71,48 @@ function num0(){
 }
 
 function numplus(){
-    if(x == 0){
-        var numbertemp= Number(number1)
-        numberanswerplus.unshift(numbertemp)
-        console.log(numberanswerplus)
-        numbertemp=0;
-    } else if (x == 1 && lastsymbol == "+"){
-        numbertemp = Number(number1)
-        numberanswerplus[0] =numberanswerplus[0]+ numbertemp; 
-        numbertemp2=numbertemp
-        numbertemp= 0;
+
+    var numbertemp= Number(number1)
+    numberanswerplus.push(numbertemp)
+    numberanswerplus.push("+")
         
-    }else if (x == 1 && lastsymbol == "-"){
-        numbertemp = Number(number1)
-        numberanswerplus[0] = numberanswerplus[0] - numbertemp2;
-    }
-
-// make numbertemp & numbertemp2 arrays
-
-
-lastsymbol = "+";
-    x=1;
+    
+numbertemp=0;
 number1 = "";
 }
 function numminus(){
-    if(x == 0){
-        var numbertemp= Number(number1)
-        numberanswerplus[0] = numbertemp
-        numbertemp2 = numbertemp;
-        numbertemp=0;
-    } else if (x == 1 && lastsymbol == "-"){
-        numbertemp = Number(number1)
-        numberanswerplus[0] = Number(numberanswerplus[0]) - numbertemp; 
-        numbertemp= 0;
-        
-    } else if (x == 1 && lastsymbol == "+"){
-        console.log(number1);
-        numbertemp = Number(number1)
-        console.log(numbertemp)
-        z++;
-        numberanswerplus[z] = numbertemp; 
-        numbertemp= 0;
-    }
-
-lastsymbol = "-";
-    x=1;
+    var numbertemp= Number(number1)
+    numberanswerplus.push(numbertemp)
+    numberanswerplus.push("-")
+    
 numbertemp=0;
 number1 = "";
-console.log(x)
 }
 function numequals(){
-    
-    if(lastsymbol =="+"){
-        numbertemp = Number(number1)
-        numberanswerplus[0] = Number(numberanswerplus[0])+numbertemp; 
-        numbertemp= 0;
-    } else if (lastsymbol =="-"){
-        numbertemp = Number(number1)
-        numberanswerplus[0] =Number(numberanswerplus[0]) - numbertemp; 
-        numbertemp= 0;
+    var numbertemp= Number(number1)
+    numberanswerplus.push(numbertemp)
+    numberanswer = numberanswerplus[0]; //remember this bigtch
+    for(let i = 1; i < numberanswerplus.length; i++){
+        if(numberanswerplus[i] == "+"){
+            tempsymbol="+"; 
+        }else if(numberanswerplus[i] == "-"){
+            tempsymbol="-";
+        }else if(numberanswerplus[i] == "x"){
+            tempsymbol="x"
+        }else if (numberanswerplus[i] == "/"){
+            tempsymbol="/"
+        }
+    if(typeof(numberanswerplus[i]) == 'number'&& tempsymbol =="+"){
+        numberanswer = Number(numberanswer) + numberanswerplus[i]
+    }else if(typeof(numberanswerplus[i])== 'number' && tempsymbol =="-"){
+        numberanswer = Number(numberanswer) - numberanswerplus[i]
+    }else if(typeof(numberanswerplus[i])== 'number' && tempsymbol =="x"){
+        numberanswer = Number(numberanswer) * numberanswerplus[i]
+    }else if(typeof(numberanswerplus[i])== 'number' && tempsymbol =="/"){
+        numberanswer = Number(numberanswer) / numberanswerplus[i]
     }
-    console.log(lastsymbol)
-    for (var i = 1; i <=z ; i++){
-        numberanswerplus[0] = Number(numberanswerplus[0]) + Number(numberanswerplus[i])
     }
+    document.getElementById("output").innerHTML = JSON.stringify(numberanswer, null, 2);
     
-    let a = numberanswerplus[0]
-    document.getElementById("output").innerHTML = JSON.stringify(a, null, 2);
-    console.log(numberanswerplus)
-    console.log(x)
     
 }
