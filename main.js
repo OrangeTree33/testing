727d69b6c4c036a877570fa1b854f8098bb3f6ac
OrangeTree33 <krakaduzaz2@gmail.com>
2022-05-15 22:34:53 +0300

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ var number1= "";
 let numberanswerplus = [];
 let numberanswer = 0;
 let tempsymbol = "";
-var intruder = new Boolean(false);
 var operations = 0;
 let testing = [];
 let x= 0;
@@ -18,49 +17,47 @@ function num1(){
 
     number1 = number1 + "1";
     document.getElementById("output").innerHTML = number1;
-    intruder = false;
+    
 }
 function num2(){
     number1= number1 + "2";
     document.getElementById("output").innerHTML = number1;
-    intruder = false;
+    
 }
 function num3(){
     number1= number1 + "3";
     document.getElementById("output").innerHTML = number1;
-    intruder = false;
+    
 }
 function num4(){
     number1= number1 + "4";
     document.getElementById("output").innerHTML = number1;
-    intruder = false;
+    
 }
 function num5(){
     number1= number1 + "5";
     document.getElementById("output").innerHTML = number1;
-    intruder = false;
+    
 }
 function num6(){
     number1= number1 + "6";
     document.getElementById("output").innerHTML = number1;
-    intruder = true; 
+    
 }
 function num7(){
     number1= number1 + "7";
     document.getElementById("output").innerHTML = number1;
-    intruder = false;
+   
 }
 function num8(){
     number1= number1 + "8";
     document.getElementById("output").innerHTML = number1;
-    intruder = false;
+   
 }
 function num9(){
     number1= number1 + "9";
     document.getElementById("output").innerHTML = number1;
-    if(intruder === true){
-        window.location.replace("mistake.html"); //PLEASE DON'T FORGET TO REMOVE WHEN FINISHEDF PROFULDCT PLEASEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
-    }
+    
 }
 function num0(){
     if(number1!=""){
