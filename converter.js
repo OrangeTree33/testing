230ddc4f1757e7347a2cce4d701fb26047375b88
OrangeTree33 <krakaduzaz2@gmail.com>
2022-05-26 19:01:48 +0300

    s

--- converter.js
+++ converter.js
@@ -1,26 +1,9 @@
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  function myFunction2() {
-    document.getElementById("myDropdown2").classList.toggle("show");
-  }
-  function myFunction3() {
+ function myFunction3() {
     document.getElementById("myDropdown3").classList.toggle("show2");
   }
-  window.onclick = function(event) {    //isjungia dropdown kai paspaudi kitur
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
-  window.onclick = function(event) {    //isjungia dropdown kai paspaudi kitur
-    if (!event.target.matches('.dropbtn')) {
+  
+  window.onclick = function(event1) {    //isjungia dropdown kai paspaudi kitur
+    if (!event1.target.matches('.dropbtn')) {
       var dropdowns = document.getElementsByClassName("dropdown-content2");
       var i;
       for (i = 0; i < dropdowns.length; i++) {
@@ -30,6 +13,16 @@ function myFunction() {
         }
       }
     }
+  }
+  
+  function svoris(){
+  
+  Element.id = "svoris1"
+  document.getElementById("ilgis1").innerHTML = Element.id
+  }
+  function turis(){
+    Element.id = "turis1"
+    document.getElementById("svoris1").innerHTML = Element.id
   }
   
   //use onkeyup for calculation start
